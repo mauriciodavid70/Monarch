d5a2bdfb211c881717e4c23bfd2d7c6e8ce6d15b
mauriciodavid70 <mauriciodavid70@hotmail.com>
2015-01-08 15:06:40 -0500

    v8 testing

---
+++ source/Monarch.Shell/Content/script.js
@@ -0,0 +1,42 @@
+﻿//expression: _this.firstName + ' ' + _this.lastName
+entity.fullName = function (_this, _root) {
+    return _this.firstName + ' ' + _this.lastName;
+}(entity, entity);
+
+//expression: _this.endDate - _this.startDate
+entity.experience.forEach(function (scope) {
+    scope.years = function (_this, _root) {
+        var diff = new Date(Math.abs(_this.endDate - _this.startDate));
+        return diff.getFullYear() - 1970;
+    }(scope, entity);
+});
+
+//aggregate expression(operation, array, expression) SUM, _this._experience, (_this.level * _this.years )
+entity.total = function (_this, _root) {
+    return _this.experience.reduce(function (acummulator, _this) {
+        acummulator += _this.level * _this.years
+        return acummulator;
+    }, 0);
+}(entity, entity);
+
+//aggregate expression(operation, array, expression) AVG, _this._experience, (_this.level * _this.years )
+entity.average = function (_this, _root) {
+    var sum = _this.experience.reduce(function (acummulator, _this) {
+        acummulator += _this.level * _this.years
+        return acummulator;
+    }, 0);
+    return sum / _this.experience.length;
+}(entity, entity);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/Monarch.Shell/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.ClearScript.V8;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monarch.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ScriptEngine_Smoke_Test();
+            JObject json = GetJObjectFromFile("Content\\candidate.json");
+            string code = GetCodeFromFile("Content\\script.js");
+
+            using (var engine = new V8ScriptEngine())
+            {
+                // expose a host type
+                engine.AddHostType("Console", typeof(Console));
+                
+                //Evaluate the json 
+                engine.Evaluate("var entity = " + json.ToString() + ";");
+                
+                ////add the candidate json
+                //engine.AddHostObject("employee", json);
+                //engine.Execute("employee.fullName = employee.firstName + ' ' + employee.lastName;Console.WriteLine(employee)");
+                engine.Execute(code);
+
+                var result = engine.Evaluate("JSON.stringify(entity)").ToString();
+
+                var jsonResult = JObject.Parse(result);
+
+                Console.WriteLine(jsonResult.ToString());
+            
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static string GetCodeFromFile(string p)
+        {
+            using (var reader = new StreamReader("Content\\script.js"))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private static JObject GetJObjectFromFile(string path)
+        {
+            using (var streamReader = new StreamReader("Content\\candidate.json"))
+            {
+                // the json text reader 
+                using (JsonTextReader reader = new JsonTextReader(streamReader))
+                {
+                    return JObject.Load(reader);
+                }
+            }
+        }
+
+        private static void ScriptEngine_Smoke_Test()
+        {
+            using (var engine = new V8ScriptEngine())
+            {
+                // expose a host type
+                engine.AddHostType("Console", typeof(Console));
+
+                var employee = new Employee { FirstName = "Andrea", LastName = "Ardila" };
+
+                engine.AddHostObject("employee", employee);
+
+                engine.Execute("employee.FullName = employee.FirstName + ' ' + employee.LastName;");
+
+                engine.Execute("Console.WriteLine('FullName = {0}', employee.FullName)");
+
+            }
+        }
+
+        public class Employee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string FullName { get; set; }
+        }
+
+        
+    }
+}
